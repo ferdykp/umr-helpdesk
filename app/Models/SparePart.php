3ec80b8e941ea36dd65f3a17efcb71b7320ced0a
Mqdd27 <ahmadkimpir@gmail.com>
2025-02-21 09:05:08 +0700

    Update Sparepaer Model

--- app/Models/SparePart.php
+++ app/Models/SparePart.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SparePart extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'sparepart';
+    protected $fillable = [
+        'nama_sparepart',
+        'kategori',
+        'stok',
+        'update_stok',
+        'lokasi_penyimpanan',
+        'status',
+        'catatan'
+    ];
 }
